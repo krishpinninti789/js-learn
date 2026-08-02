0c3417683418c46ceb50b6dd59117812504d6d71
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2026-08-02 22:44:37 +0530

    refactor: update function x to use let and implement interval logging

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+// function x() {
+//   var a = 7;
+//   return function y() {
+//     console.log(a);
+//   };
+// }
+// var z = x();
+// z();
+
 function x() {
-  var a = 7;
-  return function y() {
-    console.log(a);
-  };
+  let i = 0;
+  setInterval(() => {
+    i += 1;
+    console.log(i);
+  }, 1000);
 }
-var z = x();
-z();
+x();
