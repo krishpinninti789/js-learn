1bc17d4bcd44eb3282597f91def60276ca919dff
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2026-08-02 23:02:12 +0530

    refactor: update function x to use closure for correct timeout logging

--- index.js
+++ index.js
@@ -26,12 +26,26 @@
 
 // x();
 
+// function x() {
+//   for (var i = 0; i <= 5; i++) {
+//     setTimeout(() => {
+//       console.log(i);
+//     }, i * 1000);
+//   }
+// }
+
+// x();
+
 function x() {
-  for (var i = 0; i <= 5; i++) {
-    setTimeout(() => {
-      console.log(i);
-    }, i * 1000);
+  for (var i = 1; i <= 5; i++) {
+    function close(x) {
+      setTimeout(() => {
+        console.log(x);
+      }, x * 1000);
+    }
+    close(i);
   }
+  console.log("hi js");
 }
 
 x();
