fe0b898546f328b49b94705141c78aa740be2327
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2026-08-02 22:59:56 +0530

    refactor: replace interval logging with timeout loop for better clarity

--- index.js
+++ index.js
@@ -7,11 +7,31 @@
 // var z = x();
 // z();
 
+// function x() {
+//   let i = 0;
+//   setInterval(() => {
+//     i += 1;
+//     console.log(i);
+//   }, 1000);
+// }
+// x();
+
+// function x() {
+//   for (let i = 0; i <= 5; i++) {
+//     setTimeout(() => {
+//       console.log(i);
+//     }, i * 1000);
+//   }
+// }
+
+// x();
+
 function x() {
-  let i = 0;
-  setInterval(() => {
-    i += 1;
-    console.log(i);
-  }, 1000);
+  for (var i = 0; i <= 5; i++) {
+    setTimeout(() => {
+      console.log(i);
+    }, i * 1000);
+  }
 }
+
 x();
